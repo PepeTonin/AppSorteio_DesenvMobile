891856e9eb30d73556e76322bf7ff70d6152dc41
pedro <phavilat@hotmail.com>
2024-02-15 22:19:29 -0300

    desenvolvimento em aula

--- app/src/main/java/com/example/appsorteio/MainActivity.java
+++ app/src/main/java/com/example/appsorteio/MainActivity.java
@@ -4,24 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textNumeroSorteado;
+    EditText inputLimiteSuperiorNumeroAleatorio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inputLimiteSuperiorNumeroAleatorio = findViewById(R.id.inputNumero);
         textNumeroSorteado = findViewById(R.id.textNumeroSorteado);
+
     }
 
     public void sortearNumero(View view){
         Random gerador = new Random();
-        int numAleatorio = gerador.nextInt(11);
-        textNumeroSorteado.setText(String.valueOf(numAleatorio));
+        try {
+            int limiteSuperiorNumeroAleatorio = Integer.parseInt(inputLimiteSuperiorNumeroAleatorio.getText().toString());
+            int numAleatorio = gerador.nextInt(limiteSuperiorNumeroAleatorio);
+            textNumeroSorteado.setText(String.valueOf(numAleatorio));
+        } catch(Exception e) {
+            textNumeroSorteado.setText("Por favor, digite um numero");
+        }
     }
 }
